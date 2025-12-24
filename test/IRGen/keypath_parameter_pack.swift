e8c34322168e4cf4bff04dcde5ddc2c66de442a5
alex-reilly-dd <alex.reilly@doordash.com>
2025-12-24 12:12:11 -0800

    parameter pack keypaths working

--- test/IRGen/keypath_parameter_pack.swift
+++ test/IRGen/keypath_parameter_pack.swift
@@ -1,9 +1,8 @@
-// RUN: %target-swift-frontend -emit-ir %s -verify -target %target-swift-5.9-abi-triple
+// RUN: %target-swift-frontend -emit-ir %s -target %target-swift-5.9-abi-triple | %FileCheck %s
 
 // REQUIRES: swift_feature_ParameterPacks
 
-// Key paths on types containing parameter packs are not yet supported.
-// This test ensures we emit a diagnostic instead of crashing.
+// Test that key paths on types containing parameter packs compile successfully.
 
 struct Entry<each T> {
     let input: (repeat each T)
@@ -13,6 +12,8 @@ struct Container<each T> {
     let entries: [Entry<repeat each T>]
 
     var inputs: [(repeat each T)] {
-        entries.map(\.input) // expected-error {{unimplemented IR generation feature key paths with parameter packs}}
+        entries.map(\.input)
     }
 }
+
+// CHECK: @keypath = private global
